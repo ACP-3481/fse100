ccad1a2d413c4c2ffd9e7ea17b0838d315bb5268
ACP-3481 <70781202+ACP-3481@users.noreply.github.com>
2024-11-26 21:52:16 -0700

    Added tests for the sensors

---
+++ vibration-test.py
@@ -0,0 +1,44 @@
+import traceback
+import RPi.GPIO as GPIO
+import time
+
+VIBRATION_PIN = 18 # GPIO 24
+
+
+def setup():
+    # Set Vibration motor pin
+    GPIO.setup(VIBRATION_PIN, GPIO.OUT)
+    GPIO.output(VIBRATION_PIN, GPIO.LOW)
+
+def vibrate_on():
+    GPIO.output(VIBRATION_PIN, GPIO.HIGH)
+
+def vibrate_off():
+    GPIO.output(VIBRATION_PIN, GPIO.LOW)
+
+def loop():
+    vibrate_on()
+    time.sleep(1)
+    vibrate_off()
+    time.sleep(1)
+
+
+def destroy():
+    """
+    Deprograms the pins for safe exit
+    """
+    # turn off vibration motor
+    GPIO.output(VIBRATION_PIN, GPIO.LOW)
+
+    GPIO.cleanup()
+
+if __name__ == "__main__":
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt as exc:
+        print(exc)
+        destroy()
+    except Exception as exc:
+        traceback.print_exc()
+        destroy()
